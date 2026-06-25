f801b4ad2da78abb633e4f5dd61e47889df7f4fd
Nikita Koltcov <nickkoltcov@yandex.ru>
2026-06-25 22:07:15 +0300

    gh page

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react' // или ваш фреймворк
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/cs2mvp/', // Обязательно добавьте эту строку (имя вашего репозитория в слэшах)
 })
